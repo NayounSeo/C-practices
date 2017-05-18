1930441281c4c1fa4317bdc2bfaf2d83d27709cc
NayounSeo <parpe94@naver.com>
2017-05-19 01:36:34 +0900

    끙

--- Baekjoon/11653.cc
+++ Baekjoon/11653.cc
@@ -2,22 +2,27 @@
  * 소수 걸러내는게 문제인것 같다. */
 #include <stdio.h>
 #include <memory.h>
+#include <stdlib.h>
 #include <math.h>
+
+int eratosthenes_sieve (int* num, long *prime, long max);
 
 int main(void) {
   long N;
-  scanf("%ld", &N);
-  int *num = (int *) malloc (sizeof(int) * (N + 1));
-  memset(num, 1, sizeof(int) * (N + 1));
-  int *prime = (int *) malloc (sizeof(int));
+  scanf ("%ld", &N);
 
-  int i = 2;
-  int until = sqrt(N) > 2? sqrt(N): N;
-  while (i <= until && N > 1) {
-    printf("N : %d, i : %d\n", N, i);
-    if (N % i == 0) {
-      printf("%d\n", i);
-      N = N / i;
+  int *num = (int *) malloc (sizeof (int) * (N + 1));
+  memset (num, 1, sizeof (int) * (N + 1));
+
+  long *prime = (long *) malloc (sizeof (long));
+  int prime_cnt = eratosthenes_sieve (num, prime, N);
+
+  int i = 0;
+  while (i < prime_cnt && N > 1) {
+    // printf("current prime : %d\n", prime[i]);
+    if (N % prime[i] == 0) {
+      printf("%ld\n", prime[i]);
+      N = N / prime[i];
     } else {
       i += 1;
     }
@@ -31,22 +36,22 @@ int main(void) {
  * 에라토스테네스의 체 방법이 좋은 듯하다
  * 그리고 쓸 거라면 소수로 이루어진 배열 반환도 같이 쓰는게 좋나봐
  */
-int eratosthenes_sieve (int* num, int *prime, int max) {
-  int prime_count = 0; // 반환값
+int eratosthenes_sieve (int* num, long *prime, long max) {
+  int prime_cnt = 0; // 반환값
 
   num[0] = 0;
   num[1] = 0;
 
-  for (int i = 2; i < max; i++) {
+  for (long i = 2; i <= max; i++) {
     if (num[i]) { // 아직 지워지지 않은 수라면 소수에 등록한다.
-      prime = (int *) realloc (prime, sizeof (int) * prime_count + 1);
-      prime[prime_count] = i; 
-      prime_count += 1;
-      for (int j = 2 * i; j < max; j += i) {
+      prime = (long *) realloc (prime, sizeof (long) * prime_cnt + 1);
+      prime[prime_cnt] = i; 
+      prime_cnt += 1;
+      for (long j = 2 * i; j < max; j += i) {
         num[j] = 0;
       }
     } // end if
   }
 
-  return prime_count;
+  return prime_cnt;
 }
